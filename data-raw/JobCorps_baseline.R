04cc7f7e4a5aaaca56de7461e6dd1eb0f8fe113b
Vira Semenova <vira_semenova@fas.harvard.edu>
2019-12-03 20:56:56 -0500

    renamed data

---
+++ data-raw/JobCorps_baseline.R
@@ -0,0 +1,4 @@
+my_path<-"/net/holyparkesec/data/tata/leebounds/"
+Lee_data<-read.csv(paste0(my_path,"/JobCorps_data/dataLee2009.csv"))
+JobCorps_baseline<-Lee_data[,c("MPRID","TREATMNT.y",baseline_varnames)]
+devtools::use_data(JobCorps_baseline)
