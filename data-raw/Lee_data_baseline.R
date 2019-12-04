e0badc3270cd429003deadc50c8cbcfe9545a8c3
Vira Semenova <vira_semenova@fas.harvard.edu>
2019-12-03 19:59:41 -0500

    Removed duplicate files

--- data-raw/Lee_data_baseline.R
+++
@@ -1,4 +0,0 @@
-my_path<-"/net/holyparkesec/data/tata/leebounds/"
-Lee_data<-read.csv(paste0(my_path,"/JobCorps_data/dataLee2009.csv"))
-Lee_data_baseline<-Lee_data[,c("MPRID","TREATMNT.y",baseline_varnames)]
-devtools::use_data(Lee_data_baseline)
