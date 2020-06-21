96f5c33a4e368b1d914396e95e280a0ff6e2d7ed
Vira Semenova <vira_semenova@fas.harvard.edu>
2020-06-21 16:26:49 -0400

    step 5

--- JobCorps/STEP5_Print_Tables/print_table1.R
+++ JobCorps/STEP5_Print_Tables/print_table1.R
@@ -11,18 +11,18 @@ im<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Lee_replicated_CR_
 table4latex<-print_table(estimates,sd,im,digs=3)
 write.table(print(xtable(table4latex, type="latex"),include.rownames =FALSE ),paste0(my_path,"/JobCorps/STEP5_Print_Tables/Table1.txt"),append=FALSE)
 
-estimates<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_estimates.csv"),row.names=1)
-sd<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_CR.csv"),row.names=1)
-im<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_IM.csv"),row.names=1)
+estimates<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col123_estimates.csv"),row.names=1)
+sd<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col123_CR.csv"),row.names=1)
+im<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col123_IM.csv"),row.names=1)
 
 estimates_lasso<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_estimates_lasso.csv"),row.names=1)
 sd_lasso<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_CR_lasso.csv"),row.names=1)
 im_lasso<-read.csv(paste0(my_path,"/JobCorps/STEP5_Print_Tables/csv/Table1_Col1235_IM_lasso.csv"),row.names=1)
 
 
-estimates<-cbind(estimates[,1:6], estimates_lasso[,1:2],estimates[,7:8])
-sd<-cbind(sd[,1:6], sd_lasso[,1:2],sd[,7:8])
-im<-cbind(im[,1:6], im_lasso[,1:2],im[,7:8])
+estimates<-cbind(estimates[,1:6], estimates_lasso[,1:2])
+sd<-cbind(sd[,1:6], sd_lasso[,1:2])
+im<-cbind(im[,1:6], im_lasso[,1:2])
 
 
 
