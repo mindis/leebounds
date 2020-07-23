e8ac007b96c196af1c14927a06b931418256496a
Vira Semenova <vira_semenova@fas.harvard.edu>
2020-07-23 19:29:13 -0400

    step 4

--- JobCorps/STEP4_MISC/estimate_wage_growth.R
+++ JobCorps/STEP4_MISC/estimate_wage_growth.R
@@ -8,10 +8,29 @@ library(hdm)
 my_path<-"/net/holyparkesec/data/tata/leebounds/"
 ### load data
 sink(paste0(my_path,"/JobCorps/STEP4_MISC/Figure4.log"))
-print ("Loading data ...")
-Lee_data_covariates<-read.csv(paste0(my_path,"/JobCorps/JobCorps_data/dataLee2009.csv"))
-Lee_data<-as.data.frame(Lee_data_covariates)
+my_path<-"/net/holyparkesec/data/tata/leebounds/"
+Lee_data<-read.csv(paste0(my_path,"/JobCorps/JobCorps_data/dataLee2009.csv"))
+Lee_data<-as.data.frame(Lee_data)
+Lee_data_all_covariates<-read_feather(paste0(my_path,"/JobCorps/JobCorps_data/dataLee2009covariates.feather"))
 
+
+#Lee_data<-read_feather(paste0(my_path,"/R_code/dataLee2009.feather"))
+source(paste0(my_path,"/R/leebounds.R"))
+source(paste0(my_path,"/R/utils.R"))
+source(paste0(my_path,"/R/ortholeebounds.R"))
+source(paste0(my_path,"/R/auxiliary.R"))
+
+TREAT<-Lee_data$TREATMNT.y
+# covariates selected by David Lee (2008)
+baseline_varnames<-c("FEMALE","AGE","BLACK","HISP","OTHERRAC",
+                     "MARRIED","TOGETHER","SEPARATED","HASCHLD","NCHLD","HGC","HGC_MOTH","HGC_FATH","EVARRST",
+                     "HH_INC2","HH_INC3","HH_INC4","HH_INC5",
+                     "PERS_INC2","PERS_INC3","PERS_INC4",
+                     
+                     "CURRJOB.y", "MOSINJOB", "YR_WORK", "EARN_YR","HRSWK_JR",
+                     "WKEARNR")
+Lee_data_covariates<-Lee_data[,baseline_varnames]
+Lee_data_all_covariates<-cbind(Lee_data[,baseline_varnames],Lee_data_all_covariates)
 
 source(paste0(my_path,"/R/leebounds.R"))
 source(paste0(my_path,"/R/utils.R"))
@@ -19,37 +38,31 @@ source(paste0(my_path,"/R/ortholeebounds.R"))
 source(paste0(my_path,"/R/auxiliary.R"))
 source(paste0(my_path,"/R/orthogonal_correction.R"))
 
-### Lee's covariates
-baseline_varnames<-c("FEMALE","AGE","BLACK","HISP","OTHERRAC",
-                     "MARRIED","TOGETHER","SEPARATED","HASCHLD","NCHLD","HGC","HGC_MOTH","HGC_FATH","EVARRST",
-                     "HH_INC2","HH_INC3","HH_INC4","HH_INC5",
-                     "PERS_INC2","PERS_INC3","PERS_INC4", "CURRJOB.y", "MOSINJOB", "YR_WORK", "EARN_YR","HRSWK_JR",
-                     "WKEARNR")
-variables_for_outcome<-baseline_varnames
-Lee_data_covariates<-Lee_data[,baseline_varnames]
-Lee_data_all_covariates<-cbind(Lee_data_covariates,Lee_data[,baseline_varnames])
 
 
 
-### load functions
-source(paste0(my_path,"/R/leebounds.R"))
-source(paste0(my_path,"/R/utils.R"))
-source(paste0(my_path,"/R/ortholeebounds.R"))
-source(paste0(my_path,"/R/auxiliary.R"))
-
-
-
-
-Nboot=1000
+Nboot=300
 ci_alpha=0.05
 quantile_grid_size=0.01
 week1=104
 week2=208
 ## parametrization of half-2d shere
-phis<-seq(0,(pi),by=0.01)
+phis<-seq(0,(pi),by=0.1)
 ### selection equation specification
-selected_names<-setdiff(c(colnames(Lee_data_all_covariates)),c("treat","selection","outcome","(Intercept)","X.Intercept."))
+selected_names<-unique(c("R_GETGED1","R_COMM1","R_HOME1","PERS_INC1","PERS_INC2","HH_INC4",
+                  "HH_INC5","MOS_ANYW12"," MOS_FS12","MOS_AFDC12","EVWORKB1",
+                  "REASED_R7","HS_D1","HH144",   "AGE16",     "HGC"    ,    "FEMALE" ,    "BLACK" ,    
+                   "MOSINJOB",   "YR_WORK",    "TYPEJOBB11", "E_MATH1" ,  "FRQ_POT2" ,  "FRQ_ALC4",   "EV_ALCHL1" , "HH_INC1" ,
+                  "REASLEFT3", "WELF_KID2",  "WELF_KID4" , "WORK292" ,   "TRADWANT1" , "TALKSTAY1",  "TALK_ADL1",
+                  "E_ALONG1" ,  "KNEW_JC1",   "DRG_SUMP1" ,
+                  "PERS_INC4",  "TYPEED_R6",  "HOUS_ARR2" , "OCC_FATH8",  "F_WORK141" , "WELF_KID1" , "WORK83",
+                  "WORK422" ,   "WORK501"  , 
+                   "WORK511" ,   "WELF9AB3",   "EARN_CMP" ,  "GOVPRG_R1" , "WORK2C"  ,   "N_GUILTY6" , "HRWAGER5",
+                  "MOSINJOB5",  "WORK41"   , 
+                 "WELF5AC" ,   "WELF12ABC" ,baseline_varnames
+                  ))
 form_nonmonotone_lasso_std<-as.formula(paste0("selection~(treat)*(", paste0(selected_names,collapse="+"),")"))
+#form_nonmonotone<-as.formula("selection~treat+TSTAYCAT1+PLACEIPC5+HOWSPOKE2+TALK_CW1+R_GETGED1+R_COMM1+R_HOME1+PERS_INC1+PERS_INC2+HH_INC4+HH_INC5+MOS_ANYW12+MOS_FS12+MOS_AFDC12+HRWAGER6+OCC_R2+OCC_R9+REC_JOB2+EVWORKB1+NUMBJOBS3+REASED_R7+HS_D1+OCC_MOTH5+HH144+AGE16+AGE17+JCMSA2+WORK252+WORK3023+WORK344+WORK461+WORK5212+HGC+HGC_FATH+FEMALE+BLACK+CURRJOB.y+MOSINJOB+YR_WORK+treat:TOTALHRS1+treat:TALK_TCH1+treat:ENCR_PAR1+treat:E_READ1+treat:R_HOME1+treat:HEAR_JC5+treat:FRQ_COKE3+treat:FRQ_CIG4+treat:HEALTH2+treat:MARRIAGE1+treat:HH144+treat:NTV_LANG2+treat:NTV_LANG3+treat:SCHL11+treat:WORK313.1+treat:WORK414.1+treat:NHRSED_R+treat:OTHERRAC+TOTALHRS1+TYPEJOBB11+E_MATH1+R_HOME1+FRQ_POT2+FRQ_ALC4+PY_POT1+EV_ALCHL1+HEALTH3+HH_INC1+GOTFS11+GOTAFDC11+REASLEFT3+NCHLD3+WELF_KID2+WELF_KID4+HH141+WORK84+WORK281B+WORK292+WORK482+WELF121B+treat:TYPEWORR5+treat:CPAROLE21+treat:MARRCAT11+treat:WELF_KID2+treat:SCHL253+treat:WORK2C+TRADWANT1+TALKSTAY1+TALK_ADL1+E_ALONG1+KNEW_JC1+DRG_SUMP1+PERS_INC4+TYPEED_R6+HOUS_ARR2+OCC_FATH8+F_WORK141+WELF_KID1+WORK83+WORK422+WORK501+WORK511+WELF9AB3+EARN_CMP+treat:N_GUILTY6+treat:HRWAGER5+treat:MOSINJOB5+treat:WORK41+treat:WORK422+treat:WELF5AC+treat:WELF12ABC+IMP_CW1+IMPRCNTR4+GOVPRG_R1+HOUS_ARR1+SCHL1312+SCHL153+WORK26+WELF3123+NUMB_HH+AGE+treat:PLACEIPC6+treat:IMP_PRO1+treat:TALK_PRO1+treat:TALK_FRD1+treat:HEALTH4+treat:EVWORKB1+treat:VOC_D1+treat:HS_D1+treat:HH145+treat:JCMSA2+treat:WORK301+TOTALHRS4+IMP_PRO1+TYPEJOBB7+TYPEJOBB9+MOSTIMPR1+MOSTIMPR6+OTHMISC1+EVARRST11+MOS_TRTR1+PY_CIG1+YR_WORK11+REASED_R4+HHMEMB5+OCC_FATH7+M_WORK141+SCHL243+WORK93+WORK182+WORK222+WORK421+WORK521+WELF10AC+WELF121BC+treat:TALKSTAY1+treat:TYPEJOBB9+treat:MOSTIMPR1+treat:INFO_JC3+treat:FRQ_POT3+treat:FRQ_ALC4+treat:HH_INC5+treat:MOS_FS8+treat:REC_JOB1+treat:YR_WORK11+treat:REASLEFT3+treat:M_WORK141+treat:HH141+treat:SCHL202+treat:WORK462+treat:MOSINJOB+PLACEIPC6+ENCR_CW1+E_SPCJOB1+EV_CIG1+HEALTH4+REASLEFT1+WELF_KID3+JCMSA3+SCHL233+SCHL401B+SCHL5012+TRNG322+WORK54+WORK202+WORK37123+WORK481+WORK491+HGC_MOTH+treat:TELEMODE1+treat:IMPRCNTR4+treat:DRG_SUMP1+treat:PERS_INC2+treat:OCC_R5+treat:MOSINJOB8+treat:SCHL4912+treat:WORK3912+treat:WELF412")
 
 
 ############### ESTIMATE SELECTION EQUATION FOR WEEK 104 and 208 ####
@@ -87,7 +100,7 @@ leedata_cov1<-cbind(treat=Lee_data$TREATMNT.y,selection=selection1,outcome = out
 leedata_cov1[is.na( leedata_cov1)]<-0
 
 ### week 104: do not include baseline_varnames
-glm.fit1<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov1,selection_function = rlassologit,selection_function_name = "rlassologit")
+glm.fit1<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov1,selection_function = glm,selection_function_name = "rlassologit")
 res<-predict_selection(glm.fit1, leedata_cov1)
 s.hat=data.frame(s.0.hat=res$s.0.hat, s.1.hat=res$s.1.hat)
 p.0.star1=s.hat$s.0.hat/s.hat$s.1.hat
@@ -98,7 +111,7 @@ print (paste0("Estimating selection equation for week ",  week2))
 leedata_cov2<-cbind(treat=Lee_data$TREATMNT.y,selection=selection2,outcome = outcome2, Lee_data_all_covariates)
 leedata_cov2[is.na( leedata_cov2)]<-0
 
-glm.fit2<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov2,selection_function = rlassologit,selection_function_name = "rlassologit")
+glm.fit2<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov2,selection_function = glm,selection_function_name = "rlassologit")
 
 res<-predict_selection(glm.fit2, leedata_cov2)
 s.hat=data.frame(s.0.hat=res$s.0.hat, s.1.hat=res$s.1.hat)
@@ -143,7 +156,7 @@ for (k in 1:length(phis)) {
    
   res<-ortho_leebounds(leedata_cov=leedata_cov,s.hat=s.hat,
                                           quantile_grid_size = quantile_grid_size,
-                                          variables_for_outcome=baseline_varnames,weights=Lee_data$DSGN_WGT.y[inds_helps_helps])
+                                          variables_for_outcome=selected_names,weights=Lee_data$DSGN_WGT.y[inds_helps_helps])
   
   
   estimated_orthobounds_week104208[,k]<-GetBounds(res)
--- JobCorps/STEP4_MISC/estimated_bounds_plb.R
+++ JobCorps/STEP4_MISC/estimated_bounds_plb.R
@@ -1,54 +1,61 @@
 rm(list=ls())
-#install.packages("feather")
-library(feather)
-my_path<-"/net/holyparkesec/data/tata/leebounds/"
-Lee_data<-read.csv(paste0(my_path,"/JobCorps/JobCorps_data/dataLee2009.csv"))
-Lee_data<-as.data.frame(Lee_data)
-Lee_data_all_covariates<-read_feather(paste0(my_path,"/JobCorps/JobCorps_data/dataLee2009covariates.feather"))
 
-#Lee_data<-read_feather(paste0(my_path,"/R_code/dataLee2009.feather"))
+## load packages
+print ("Loading packages ...")
+library(sas7bdat)
+library(SDMTools)
+library(expm)
+library(feather)
+library(hdm)
+
+my_path<-"/net/holyparkesec/data/tata/leebounds/"
+### load data
+#sink(paste0(my_path,"/JobCorps/STEP3_Estimate_Bounds/Table1_Col4_lasso.log"))
+print ("Loading data ...")
+Lee_data_covariates<-read.csv(paste0(my_path,"/JobCorps_data/dataLee2009.csv"))
+Lee_data_all_covariates<-read_feather(paste0(my_path,"/JobCorps_data/dataLee2009covariates3.feather"))
+Lee_data<-as.data.frame(Lee_data_covariates)
+
 source(paste0(my_path,"/R/leebounds.R"))
 source(paste0(my_path,"/R/utils.R"))
 source(paste0(my_path,"/R/ortholeebounds.R"))
 source(paste0(my_path,"/R/auxiliary.R"))
 source(paste0(my_path,"/R/orthogonal_correction.R"))
-### FIGURE 1 and 2###
+source(paste0(my_path,"/JobCorps/STEP3_Estimate_Bounds/utils.R"))
+selected_weeks<-c(45,90,104,135,180,208)
 weeks<-1:208
+N_rep=300
+ci_alpha=0.05
 quantile_grid_size=0.01
-### FIGURE 5
-s.hat.nonmonotone.glm<-array(0,c(dim(Lee_data_all_covariates)[1],2,length(weeks)))
-estimated.p.0.hat.nonmonotone<-matrix(0,dim(Lee_data_all_covariates)[1],length(weeks))
-bounds_non_treated_wage<-matrix(0,2,length(weeks))
+
+orthoestimates_plb<-matrix(0,2,length(weeks))
 frac_positive<-rep(0,length(weeks))
-estimates_plb<-matrix(0,2,length(weeks))
-
-
-
-TREAT<-Lee_data$TREATMNT.y
+#### Estimate selection equation
 baseline_varnames<-c("FEMALE","AGE","BLACK","HISP","OTHERRAC",
                      "MARRIED","TOGETHER","SEPARATED","HASCHLD","NCHLD","HGC","HGC_MOTH","HGC_FATH","EVARRST",
                      "HH_INC2","HH_INC3","HH_INC4","HH_INC5",
-                     "PERS_INC2","PERS_INC3","PERS_INC4",
-                     
-                     "CURRJOB.y", "MOSINJOB", "YR_WORK", "EARN_YR","HRSWK_JR",
-                     "WKEARNR"  )
+                     "PERS_INC2","PERS_INC3","PERS_INC4", "CURRJOB.y", "MOSINJOB", "YR_WORK", "EARN_YR","HRSWK_JR",
+                     "WKEARNR")
+Lee_data_covariates<-Lee_data[,baseline_varnames]
 
-Lee_data_all_covariates<-cbind(Lee_data[,baseline_varnames],Lee_data_all_covariates)
+selected_covs_selection<-list()
+selected_covs_outcome<-list()
 
-
-selected_names<-setdiff(colnames(Lee_data_all_covariates),c("treat","selection","outcome","(Intercept)","X.Intercept."))
-form_nonmonotone<-as.formula("selection~treat+TSTAYCAT1+PLACEIPC5+HOWSPOKE2+TALK_CW1+R_GETGED1+R_COMM1+R_HOME1+PERS_INC1+PERS_INC2+HH_INC4+HH_INC5+MOS_ANYW12+MOS_FS12+MOS_AFDC12+HRWAGER6+OCC_R2+OCC_R9+REC_JOB2+EVWORKB1+NUMBJOBS3+REASED_R7+HS_D1+OCC_MOTH5+HH144+AGE16+AGE17+JCMSA2+WORK252+WORK3023+WORK344+WORK461+WORK5212+HGC+HGC_FATH+FEMALE+BLACK+CURRJOB.y+MOSINJOB+YR_WORK+treat:TOTALHRS1+treat:TALK_TCH1+treat:ENCR_PAR1+treat:E_READ1+treat:R_HOME1+treat:HEAR_JC5+treat:FRQ_COKE3+treat:FRQ_CIG4+treat:HEALTH2+treat:MARRIAGE1+treat:HH144+treat:NTV_LANG2+treat:NTV_LANG3+treat:SCHL11+treat:WORK313.1+treat:WORK414.1+treat:NHRSED_R+treat:OTHERRAC+TOTALHRS1+TYPEJOBB11+E_MATH1+R_HOME1+FRQ_POT2+FRQ_ALC4+PY_POT1+EV_ALCHL1+HEALTH3+HH_INC1+GOTFS11+GOTAFDC11+REASLEFT3+NCHLD3+WELF_KID2+WELF_KID4+HH141+WORK84+WORK281B+WORK292+WORK482+WELF121B+treat:TYPEWORR5+treat:CPAROLE21+treat:MARRCAT11+treat:WELF_KID2+treat:SCHL253+treat:WORK2C+TRADWANT1+TALKSTAY1+TALK_ADL1+E_ALONG1+KNEW_JC1+DRG_SUMP1+PERS_INC4+TYPEED_R6+HOUS_ARR2+OCC_FATH8+F_WORK141+WELF_KID1+WORK83+WORK422+WORK501+WORK511+WELF9AB3+EARN_CMP+treat:N_GUILTY6+treat:HRWAGER5+treat:MOSINJOB5+treat:WORK41+treat:WORK422+treat:WELF5AC+treat:WELF12ABC+IMP_CW1+IMPRCNTR4+GOVPRG_R1+HOUS_ARR1+SCHL1312+SCHL153+WORK26+WELF3123+NUMB_HH+AGE+treat:PLACEIPC6+treat:IMP_PRO1+treat:TALK_PRO1+treat:TALK_FRD1+treat:HEALTH4+treat:EVWORKB1+treat:VOC_D1+treat:HS_D1+treat:HH145+treat:JCMSA2+treat:WORK301+TOTALHRS4+IMP_PRO1+TYPEJOBB7+TYPEJOBB9+MOSTIMPR1+MOSTIMPR6+OTHMISC1+EVARRST11+MOS_TRTR1+PY_CIG1+YR_WORK11+REASED_R4+HHMEMB5+OCC_FATH7+M_WORK141+SCHL243+WORK93+WORK182+WORK222+WORK421+WORK521+WELF10AC+WELF121BC+treat:TALKSTAY1+treat:TYPEJOBB9+treat:MOSTIMPR1+treat:INFO_JC3+treat:FRQ_POT3+treat:FRQ_ALC4+treat:HH_INC5+treat:MOS_FS8+treat:REC_JOB1+treat:YR_WORK11+treat:REASLEFT3+treat:M_WORK141+treat:HH141+treat:SCHL202+treat:WORK462+treat:MOSINJOB+PLACEIPC6+ENCR_CW1+E_SPCJOB1+EV_CIG1+HEALTH4+REASLEFT1+WELF_KID3+JCMSA3+SCHL233+SCHL401B+SCHL5012+TRNG322+WORK54+WORK202+WORK37123+WORK481+WORK491+HGC_MOTH+treat:TELEMODE1+treat:IMPRCNTR4+treat:DRG_SUMP1+treat:PERS_INC2+treat:OCC_R5+treat:MOSINJOB8+treat:SCHL4912+treat:WORK3912+treat:WELF412")
-#form_nonmonotone<-as.formula(paste0("selection~(treat)*(", paste0(baseline_varnames,collapse="+"),")"))
-
-s.hat.nonmonotone.glm<-array(0,c(dim(Lee_data_all_covariates)[1],2,length(weeks)))
-fraction_treat_helps<-rep(0,length(weeks))
-variables_for_outcome<-baseline_varnames
+my_names<-setdiff(colnames(Lee_data_all_covariates),c("treat","selection","outcome","(Intercept)","X.Intercept.","MPRID" ))
+form_nonmonotone_lasso_std<-as.formula(paste0("selection~(treat)*(", paste0(my_names,collapse="+"),")"))
+covs<-list()
+prob_helps<-rep(0,6)
 
 
+is_positive_list<-list()
+treatment_effect_lower_bound_list<-list()
 
-for (i in 133:length(weeks)) {
-  print(i)
+### step 1: find subjects with positive lower bound in each week
+for (i in 80:120) {
   week<-weeks[i]
+  
+  print (paste0("Results for week ", week))
+  
   hwh_name<-paste0("HWH",week)
   earn_name<-paste0("EARNH",week)
   
@@ -56,39 +63,126 @@ for (i in 133:length(weeks)) {
   logwage_week[is.na(logwage_week)]<-0
   logwage_week[logwage_week==-Inf]<-0
   
-  leedata_week<-as.data.frame(cbind(treat=Lee_data$TREATMNT.y,selection=as.numeric(Lee_data[,hwh_name]>0),outcome=logwage_week))
+  leedata_week<-as.data.frame(cbind(treat=Lee_data$TREATMNT.y,selection=as.numeric(Lee_data[,earn_name]>0),outcome=logwage_week))
   
-  leedata_cov<-cbind(treat=Lee_data$TREATMNT.y,selection=logwage_week>0,outcome = logwage_week, Lee_data_all_covariates)
+  leedata_cov<-cbind(treat=Lee_data$TREATMNT.y,selection=as.numeric(Lee_data[,earn_name]>0),outcome = logwage_week, Lee_data_all_covariates,
+                     Lee_data[,baseline_varnames])
   leedata_cov[is.na( leedata_cov)]<-0
-  ## analysis without monotonicity
-  glm.fit<-glm(form=form_nonmonotone,data= leedata_cov[,setdiff(colnames(leedata_cov),"outcome")],family="binomial")
-  res<-predict_selection(glm.fit, leedata_cov)
-  s.hat=data.frame(s.0.hat=res$s.0.hat, s.1.hat=res$s.1.hat)
-  s.hat.nonmonotone.glm[,,i]<-as.matrix(s.hat,ncol=2)
-  estimated.p.0.hat.nonmonotone[,i]<-s.hat$s.0.hat/s.hat$s.1.hat
   
+  leedata_week$weights<-Lee_data$DSGN_WGT.y
+  leedata_cov$weights<-Lee_data$DSGN_WGT.y
+  min_wage=min(leedata_cov$outcome[leedata_cov$selection==1])
+  max_wage=max(leedata_cov$outcome[leedata_cov$selection==1])
+  
+  
+  ## monotonicity-preserving bounds ##
+  glm.fit<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov,selection_function_name = "rlassologit",
+                              penalty=list(lambda=300)
+  )
+  covs[[i]]<-names(glm.fit$coefficients)
+  
+  selected_names<-setdiff( c(setdiff(covs[[i]],grep("treat:",covs[[i]],value=TRUE)),unlist(strsplit(grep("treat:",covs[[i]],value=TRUE),"treat:"))),   c("","treat","(Intercept)") )
+  
+  form_nonmonotone_ss<-as.formula(paste0("selection~treat*(",paste0(selected_names,collapse="+"), ")*(",paste0(selected_names,collapse="+"),")" ))
+  
+  
+  glm.fit<-estimate_selection(form=form_nonmonotone_ss,leedata=leedata_cov,selection_function_name = "rlassologit",
+                              names_to_include=c("treat:EARN_YR","treat:R_HOME1"),
+                              penalty=list(lambda=850))
+  
+  s.hat<-as.data.frame(predict_selection(glm.fit,leedata_cov))
+  p.0.star<-s.hat$s.0.hat/s.hat$s.1.hat
+  inds_helps<-(p.0.star<=1)
+  inds_hurts<-(!inds_helps)
+  prob_helps[i]<-mean(p.0.star<1)
   
   leebounds_ortho_result<-first_stage_wrapper(leedata_cov,weights=Lee_data$DSGN_WGT.y,
-                                              variables_for_outcome=baseline_varnames,
+                                              variables_for_outcome=c("EARN_YR","EARN_CMP","FEMALE","BLACK","PERS_INC1","PERS_INC4","HH_INC5","WKEARNR"),
                                               quantile_grid_size=0.01,
                                               sort_quantiles=TRUE,
                                               s.hat=s.hat)
   
-  
-  res<-summary_subjects_positive_lower_bound(leedata_cov_total=leedata_cov[,c("treat","selection","outcome",baseline_varnames)],
+  selected_vars<-c(baseline_varnames,"EARN_CMP")
+  res<-treatment_effect_lower_bound(leedata_cov_total=leedata_cov[,c("treat","selection","outcome",selected_vars)],
                                              s.hat=leebounds_ortho_result$s.hat,y.hat=leebounds_ortho_result$y.hat,
                                              
                                              quantile_grid_size=quantile_grid_size,
                                              variables_for_outcome=baseline_varnames,
-                                             form_outcome=paste0("outcome~",paste0(baseline_varnames,collapse="+")),weights=Lee_data$DSGN_WGT.y)
-  estimates_plb[,i]<-GetBounds(res)
-  frac_positive[i]<-GetFraction(res)
+                                             form_outcome=paste0("outcome~",paste0(selected_vars,collapse="+")),weights=Lee_data$DSGN_WGT.y,
+                                             cutoff_positive=0.001)
+  
+  treatment_effect_lower_bound_list[[i]]<-res$treat_effect_lower_bound
+}
+## define target population of interest: positive lower bound at any week in (45,90,104,135,180,208)
+ps_any_week<-apply(matrix(unlist(treatment_effect_lower_bound_list),nrow=9145),1,mean)>0
+for (i in 5:208) {
+  
+  week<-weeks[i]
+  
+  print (paste0("Results for week ", week))
+  
+  hwh_name<-paste0("HWH",week)
+  earn_name<-paste0("EARNH",week)
+  
+  logwage_week<-as.numeric(as.matrix(log(Lee_data[,earn_name]/Lee_data[,hwh_name])))
+  logwage_week[is.na(logwage_week)]<-0
+  logwage_week[logwage_week==-Inf]<-0
+  
+  leedata_week<-as.data.frame(cbind(treat=Lee_data$TREATMNT.y,selection=as.numeric(Lee_data[,earn_name]>0),outcome=logwage_week))
+  
+  leedata_cov<-cbind(treat=Lee_data$TREATMNT.y,selection=as.numeric(Lee_data[,earn_name]>0),outcome = logwage_week, Lee_data_all_covariates,
+                     Lee_data[,baseline_varnames])
+  leedata_cov[is.na( leedata_cov)]<-0
+  
+  leedata_week$weights<-Lee_data$DSGN_WGT.y
+  leedata_cov$weights<-Lee_data$DSGN_WGT.y
+  min_wage=min(leedata_cov$outcome[leedata_cov$selection==1])
+  max_wage=max(leedata_cov$outcome[leedata_cov$selection==1])
+  
+  
+  ## monotonicity-preserving bounds ##
+  glm.fit<-estimate_selection(form=form_nonmonotone_lasso_std,leedata=leedata_cov,selection_function_name = "rlassologit",
+                              penalty=list(lambda=300)
+  )
+  covs[[i]]<-names(glm.fit$coefficients)
+  
+  selected_names<-setdiff( c(setdiff(covs[[i]],grep("treat:",covs[[i]],value=TRUE)),unlist(strsplit(grep("treat:",covs[[i]],value=TRUE),"treat:"))),   c("","treat","(Intercept)") )
+  
+  form_nonmonotone_ss<-as.formula(paste0("selection~treat*(",paste0(selected_names,collapse="+"), ")*(",paste0(selected_names,collapse="+"),")" ))
+  
+  
+  #selected_covs_selection[[i]]<-unique(c(baseline_varnames,paste0("treat:",baseline_varnames),"treat:EARN_CMP","treat:R_HOME1" ))
+  glm.fit<-estimate_selection(form=form_nonmonotone_ss,leedata=leedata_cov,selection_function_name = "rlassologit",
+                              names_to_include=unique(c(baseline_varnames,paste0("treat:",baseline_varnames) )),
+                              penalty=list(lambda=850))
+  
+  s.hat<-as.data.frame(predict_selection(glm.fit,leedata_cov))
+  p.0.star<-s.hat$s.0.hat/s.hat$s.1.hat
+  inds_helps<-(p.0.star<=1)
+  inds_hurts<-(!inds_helps)
+  prob_helps[i]<-mean(p.0.star<1)
+  
+  ### first stage is estimated on the full sample
+  fs_result<-first_stage_wrapper(leedata_cov,weights=Lee_data$DSGN_WGT.y,
+                                              variables_for_outcome=c("EARN_YR","EARN_CMP","FEMALE","BLACK","PERS_INC1","PERS_INC4","HH_INC5","WKEARNR"),
+                                              quantile_grid_size=0.01,
+                                              sort_quantiles=TRUE,
+                                              s.hat=s.hat)
+  ## bounds (moment functions) are evaluated for a subset ps_any_week
+  leebounds_ortho_result<-second_stage_wrapper(leedata=leedata_cov[  ps_any_week ,],
+                                                inds_helps=inds_helps[ps_any_week],
+                                                s.hat=  fs_result$s.hat[  ps_any_week,],
+                                                y.hat= fs_result$y.hat[ps_any_week,],
+                                                weight=Lee_data$DSGN_WGT.y[ps_any_week],ortho=TRUE,c_quant=0) 
+  orthoestimates_plb[,i]<-GetBounds(leebounds_ortho_result)
+  ## week-specific bounds
+  
   
 }
 selected_weeks<-5:208
 Figure3_dataset<-data.frame(weeks=weeks[selected_weeks],
-                            lb_positive=estimates_plb[1,selected_weeks],
-                            ub_positive=estimates_plb[2,selected_weeks],
-                            fraction=frac_positive[selected_weeks]
+                            lb_positive=orthoestimates_plb[1,selected_weeks],
+                            ub_positive=orthoestimates_plb[2,selected_weeks]
+                            
 )
-write.csv(Figure3_dataset,paste0(my_path,"/JobCorps/STEP5_Print_Figures/csv/Figure3_selected_covs.csv"))
+write.csv(Figure3_dataset,paste0(my_path,"/JobCorps/STEP5_Print_Figures/csv/Figure3_selected_covs3.csv"))
